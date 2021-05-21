b8ca517fcbf0a58f41e6731f2510ea70fcaa3cc3
Niklas Moser <niklas.moser@students.uni-freiburg.de>
2021-05-21 15:33:49 +0200

    initial commit

---
+++ code/utils.py
@@ -0,0 +1,36 @@
+# !/usr/bin/env python
+# coding: utf-8
+import math
+import numpy as np
+import pandas as pd
+import torch
+
+def normalize(var, scaling=None):
+    '''
+    This function normalizes variables around the mean and the standard deviation
+    :param var: two dimensional array of data points to normalize e.g. pd.DataFrame, torch.tensor
+    :param scaling: other targets to normalize on
+    :return: scaled variables in 2-D array
+    '''
+    if not scaling:
+        if (isinstance(var, pd.DataFrame)):
+            out = (var - np.mean(var)) / np.std(var)
+        elif (torch.is_tensor(var)):
+            out = (var - torch.mean(var)) / torch.std(var)
+        else:
+            out = (var - np.mean(var, axis=0)) / np.std(var, axis=0)
+    else:
+        out = (var - scaling[0]) / scaling[1]
+
+    return out
+
+
+def encode_doy(doy):
+    '''
+    Encode cyclic feature as doy [1, 365] or [1, 366]
+    :param doy: cyclic feature e.g. doy [1, 365]
+    :return: encoded feature in sine and cosine
+    '''
+    normalized_doy = doy * (2. * np.pi / 365)
+    return np.sin(normalized_doy), np.cos(normalized_doy)
+
