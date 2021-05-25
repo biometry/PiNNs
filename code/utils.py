db67c95428b60a69bc3e71dc23c20d6ea0ccd337
Niklas Moser <niklas.moser@students.uni-freiburg.de>
2021-05-25 18:38:55 +0200

    add DataLoader

--- code/utils.py
+++ code/utils.py
@@ -1,8 +1,8 @@
 # !/usr/bin/env python
 # coding: utf-8
-import math
 import numpy as np
 import pandas as pd
+import random
 import torch
 
 def standardize(var, scaling=None):
@@ -33,4 +33,52 @@ def encode_doy(doy):
     '''
     normalized_doy = doy * (2. * np.pi / 365)
     return np.sin(normalized_doy), np.cos(normalized_doy)
+
+
+def add_history(X, Y, batch_size, history):
+    '''
+    ref: Marieke Wesselkamp
+    Mini-batches for training
+    :param X: PRELES Inputs, standardized
+    :param Y: Observed GPP and ET
+    :param batch_size: batch size
+    :param history: data points from which time scale before should be used
+    :return: x and y
+    '''
+    subset = [j for j in random.sample(range(X.shape[0]), batch_size) if j > history]
+    subset_h = [item for sublist in [list(range(j - history, j)) for j in subset] for item in sublist]
+    x = np.concatenate((X.iloc[subset], X.iloc[subset_h]), axis=0)
+    y = np.concatenate((Y.iloc[subset], Y.iloc[subset_h]), axis=0)
+
+    return x, y
+
+
+def read_in(type, data_dir=None):
+    if not data_dir:
+        data_path = 'C:/Users/Niklas/Desktop/Uni/M.Sc. Environmental Science/Thesis/physics_guided_nn/data/'
+    else:
+        data_path = data_dir
+    # subset station
+    if type == 'NAS':
+        out = pd.read_csv(''.join((data_path, 'soro.csv')))
+    return out
+
+
+
+def DataLoader(data_split, batch_size, history, dir=None):
+    xcols = ['PAR', 'Tair', 'VPD', 'Precip', 'fapar', 'doy_sin', 'doy_cos']
+    ycols = ['GPP', 'ET']
+    if data_split == 'NAS':
+        data = read_in(data_split, dir)
+        data['doy_sin'], data['doy_cos'] = encode_doy(data['DOY'])
+        data = standardize(data.drop(['CO2', 'date', 'DOY'], axis=1))
+
+    x, y = add_history(data[xcols], data[ycols], batch_size, history)
+
+
+    return x, y
+
+
+
+
 
