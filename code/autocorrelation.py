5dbe9982961f908729bfe18bdebeba418cdfffe8
Niklas Moser <niklas.moser@students.uni-freiburg.de>
2021-05-25 14:45:51 +0200

    create partial AC plots

--- code/autocorrelation.py
+++ code/autocorrelation.py
@@ -2,10 +2,14 @@
 # coding: utf-8
 import matplotlib.pyplot as plt
 import pandas as pd
-from statsmodels.graphics.tsaplots import plot_pacf
+from statsmodels.graphics.tsaplots import plot_pacf, plot_acf
 
 data_path = 'C:/Users/Niklas/Desktop/Uni/M.Sc. Environmental Science/Thesis/physics_guided_nn/data/'
-data = pd.read_csv(''.join((data_path, 'soro.csv')))
+soro = pd.read_csv(''.join((data_path, 'soro.csv')))
+hyytiala = pd.read_csv(''.join((data_path, 'hyytiala.csv')))
+collelongo = pd.read_csv(''.join((data_path, 'collelongo.csv')))
 
-plot_pacf(data['Precip'])
+plot_pacf(collelongo['Tair'], lags=30)
+plot_pacf(soro['Tair'], lags=30)
+plot_pacf(hyytiala['Tair'], lags=30)
 plt.show()
