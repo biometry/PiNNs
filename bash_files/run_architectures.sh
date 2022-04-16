f5031384e13a6c61d694cc8adac9112a43edf757
Niklas Moser <84136721+therealniklasmoser@users.noreply.github.com>
2022-04-16 17:42:30 +0300

    Update run_architectures.sh

--- bash_files/run_architectures.sh
+++ bash_files/run_architectures.sh
@@ -15,8 +15,8 @@ do
 	    touch $FILE
 	fi
 
-	echo -e "#!/bin/sh\n#SBATCH --job-name NAS_EX1_${i}_${d}\n#SBATCH --account=Project_2000527\n#SBATCH --time=10-00:15:00\n#SBATCH --mem-per-cpu=100G\n#SBATCH --partition=longrun\n" >> $FILE
-	echo -e "source /projappl/project_2000527/miniconda3/etc/profile.d/conda.sh\nconda activate pgnn\ncd /users/mosernik/physics_guided_nn\npython code/EN${i}.py -d ${d}\nconda deactivate" >> $FILE
+	echo -e "#!/bin/sh\n#SBATCH --job-name NAS_EX1_${i}_${d}\n#SBATCH --account=a_project_number\n#SBATCH --time=10-00:15:00\n#SBATCH --mem-per-cpu=100G\n#SBATCH --partition=longrun\n" >> $FILE
+	echo -e "source /projappl/a_project_number/miniconda3/etc/profile.d/conda.sh\nconda activate pgnn\ncd /users/mosernik/physics_guided_nn\npython code/EN${i}.py -d ${d}\nconda deactivate" >> $FILE
 	sbatch $FILE
     done
 done    
