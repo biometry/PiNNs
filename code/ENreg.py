a3bbeb62dfa68b39ec8d35f917d7998aa863645e
Niklas Moser <mosernik@puhti-login1.bullx>
2022-08-15 17:32:56 +0300

    extensive NAS

--- code/ENreg.py
+++ code/ENreg.py
@@ -7,13 +7,14 @@ import torch
 import pandas as pd
 import numpy as np
 import argparse
+import HPe
 
 parser = argparse.ArgumentParser(description='Define data usage and splits')
 parser.add_argument('-d', metavar='data', type=str, help='define data usage: full vs sparse')
 #parser.add_argument('-s', metavar='splits', type=int, help='define number of splits')
 args = parser.parse_args()
 
-def ENreg(data_use='full', splits=None):
+def ENreg(data_use='full', v=2, splits=None):
     if data_use == 'sparse':
         x, y, xt = utils.loaddata('NAS', 1, dir="./data/", raw=True, sparse=True)
     else:
@@ -30,22 +31,26 @@ def ENreg(data_use='full', splits=None):
     print("INPUTS: \n", x, "Outputs: \n", y, "RAW DATA: \n", reg)
     x.index, y.index, reg.index = np.arange(0, len(x)), np.arange(0, len(y)), np.arange(0, len(reg))
 
+    if v==1:
+        arch_grid = HP.ArchitectureSearchSpace(x.shape[1], y.shape[1], 800, 4)
 
-    arch_grid = HP.ArchitectureSearchSpace(x.shape[1], y.shape[1], 800, 4)
+        # architecture search
+        layersizes, ag = HP.ArchitectureSearch(arch_grid, {'epochs': 200, 'batchsize': 8, 'lr':0.001, "eta": 0.2}, x, y, splits, "arSreg", reg, hp=True)
+        ag.to_csv(f"/scratch/project_2000527/pgnn/results/NregAS_{data_use}.csv")
 
-    # architecture search
-    layersizes, ag = HP.ArchitectureSearch(arch_grid, {'epochs': 200, 'batchsize': 8, 'lr':0.001, "eta": 0.2}, x, y, splits, "arSreg", reg, hp=True)
-    ag.to_csv(f"/scratch/project_2000527/pgnn/results/NregAS_{data_use}.csv")
+        # Hyperparameter Search Space
+        hpar_grid = HP.HParSearchSpace(800, True)
 
-    # Hyperparameter Search Space
-    hpar_grid = HP.HParSearchSpace(800, True)
+        # Hyperparameter search
+        hpars, grid = HP.HParSearch(layersizes, hpar_grid, x, y, splits, "hpreg", reg, hp=True)
 
-    # Hyperparameter search
-    hpars, grid = HP.HParSearch(layersizes, hpar_grid, x, y, splits, "hpreg", reg, hp=True)
+        print( 'hyperparameters: ', hpars)
+        grid.to_csv(f"/scratch/project_2000527/pgnn/results/NregHP_{data_use}.csv")
 
-    print( 'hyperparameters: ', hpars)
-    grid.to_csv(f"/scratch/project_2000527/pgnn/results/NregHP_{data_use}.csv")
-
+    elif v==2:
+        arch_grid, par_grid = HPe.NASSearchSpace(x.shape[1], y.shape[1], 100, 100, 4, reg=True)
+        res = HPe.NASSearch(arch_grid, par_grid, x, y, splits, "NASreg", reg=reg, hp=True)
+        res.to_csv(f"/scratch/project_2000527/pgnn/results/NregHP_{data_use}_new.csv")
 
 if __name__ == '__main__':
     ENreg(args.d)
--- code/ENres.py
+++ code/ENres.py
@@ -7,6 +7,7 @@ import torch
 import pandas as pd
 import numpy as np
 import argparse
+import HPe
 
 parser = argparse.ArgumentParser(description='Define data usage and splits')
 parser.add_argument('-d', metavar='data', type=str, help='define data usage: full vs sparse')
@@ -14,7 +15,7 @@ parser.add_argument('-d', metavar='data', type=str, help='define data usage: ful
 args = parser.parse_args()
 
 
-def ENres(data_use="full"):
+def ENres(data_use="full", v=2):
     
     if data_use == 'sparse':
         x, y, xt = utils.loaddata('NASp', 1, dir="./data/", raw=True, sparse=True)
@@ -30,26 +31,29 @@ def ENres(data_use="full"):
     
     x.index, y.index = np.arange(0, len(x)), np.arange(0, len(y))
     
-    # orignial grid size: 800
-    arch_grid = HP.ArchitectureSearchSpace(x.shape[1], y.shape[1], 800, 4)
+    if v==1:
+        # orignial grid size: 800
+        arch_grid = HP.ArchitectureSearchSpace(x.shape[1], y.shape[1], 800, 4)
     
-    # architecture search
-    layersizes, agrid = HP.ArchitectureSearch(arch_grid, {'epochs': 200, 'batchsize': 8, 'lr':0.001}, x, y, splits, "arSres", hp=True)
+        # architecture search
+        layersizes, agrid = HP.ArchitectureSearch(arch_grid, {'epochs': 200, 'batchsize': 8, 'lr':0.001}, x, y, splits, "arSres", hp=True)
     
-    agrid.to_csv(f"/scratch/project_2000527/pgnn/results/NresAS_{data_use}.csv")
+        agrid.to_csv(f"/scratch/project_2000527/pgnn/results/NresAS_{data_use}.csv")
     
-    # Hyperparameter Search Space
-    # original search space size: 800
-    hpar_grid = HP.HParSearchSpace(800)
+        # Hyperparameter Search Space
+        # original search space size: 800
+        hpar_grid = HP.HParSearchSpace(800)
     
-    # Hyperparameter search
-    hpars, grid = HP.HParSearch(layersizes, hpar_grid, x, y, splits, "hpres", hp=True)
+        # Hyperparameter search
+        hpars, grid = HP.HParSearch(layersizes, hpar_grid, x, y, splits, "hpres", hp=True)
     
-    print( 'hyperparameters: ', hpars)
-    
-    
-    grid.to_csv(f"/scratch/project_2000527/pgnn/results/NresHP_{data_use}.csv")
+        print( 'hyperparameters: ', hpars)
+        grid.to_csv(f"/scratch/project_2000527/pgnn/results/NresHP_{data_use}.csv")
 
+    elif v==2:
+        arch_grid, par_grid = HPe.NASSearchSpace(x.shape[1], y.shape[1], 100, 100, 4)
+        res = HPe.NASSearch(arch_grid, par_grid, x, y, splits, "hpmlp", hp=True)
+        
 if __name__ == '__main__':
     ENres(args.d)
 
