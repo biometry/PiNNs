b2560b0d1964d814cba90ba84adf8615ece743a0
Niklas Moser <mosernik@r18c02.bullx>
2024-10-05 17:26:06 +0300

    temp nas

--- temporal/nas/ENmlp.py
+++ temporal/nas/ENmlp.py
@@ -16,24 +16,23 @@ parser.add_argument('-d', metavar='data', type=str, help='define data usage: ful
 args = parser.parse_args()
 
 def ENmlp(data_use="full", splits=None):
-    print('pre data-loading')
+    
     if data_use == 'sparse':
         x, y, xt = utils.loaddata('NAS', 1, dir="../../data/", raw=True, sparse=True)
     else:
         x, y, xt = utils.loaddata('NAS', 1, dir="../../data/", raw=True)
     y = y.to_frame()
 
+
         
     if splits is None:
-        splits = len(x.index.year.unique())
-    
-    
+        splits = len(x.index.year.unique())    
     
     x.index, y.index = np.arange(0, len(x)), np.arange(0, len(y))
         
     arch_grid, par_grid = HP.NASSearchSpace(x.shape[1], y.shape[1], 300, 300, 4)
     res = HP.NASSearch(arch_grid, par_grid, x, y, splits, "NASmlp", hp=True)
 
-    res.to_csv(f"/results/NmlpHP_{data_use}.csv")
+    res.to_csv(f"./results/NmlpHP_{data_use}.csv")
 if __name__ == '__main__':
     ENmlp(args.d)
