babe84ee8bc19dfc6342b8f2f9dafccb8d844c4e
Niklas Moser <mosernik@r18c02.bullx>
2024-10-05 17:30:03 +0300

    spatial nas

--- spatial/nas/EN2reg.py
+++ spatial/nas/EN2reg.py
@@ -1,4 +1,3 @@
-
 # !/usr/bin/env python
 # coding: utf-8
 # @author: Niklas Moser
@@ -31,7 +30,6 @@ def EN2reg(data_use='full'):
     y = y.to_frame()
 
     splits = 5
-    print(x,y, reg)
     
     x.index, y.index, reg.index = np.arange(0, len(x)), np.arange(0, len(y)), np.arange(0, len(reg))
     arch_grid, par_grid = HP.NASSearchSpace(x.shape[1], y.shape[1], 300, 300, 4, reg=True)
