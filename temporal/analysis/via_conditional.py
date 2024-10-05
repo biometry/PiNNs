c39575c186f244a2dbb25b5455d3c6249d6db9d2
Niklas Moser <mosernik@r18c02.bullx>
2024-10-05 18:05:56 +0300

    temporal via

--- temporal/analysis/via_conditional.py
+++ temporal/analysis/via_conditional.py
@@ -11,12 +11,12 @@ import pandas as pd
 import numpy as np
 import argparse
 
-parser = argparse.ArgumentParser(description='Define data usage and splits')
+parser = argparse.ArgumentParser(description='Define data usage')
 parser.add_argument('-d', metavar='data', type=str, help='define data usage: full vs sparse')
 parser.add_argument('-m', metavar='model', type=str, help='define model: mlp, res, res2, reg, emb, da')
 args = parser.parse_args()
 
-def predict(test_x, test_y, m, data_use, yp, xt_test, current_dir=''):
+def predict(test_x, test_y, m, data_use, yp, xt_test, current_dir='', N=5000, da=1):
     # Architecture
     if m == 'mlpDA':
         res_as = pd.read_csv(f"../nas/results/NmlpHP_{data_use}.csv")
@@ -35,7 +35,8 @@ def predict(test_x, test_y, m, data_use, yp, xt_test, current_dir=''):
     data_dir = "../models/"
     print("DATA",test_x, test_y, yp)
     test_x, test_y = torch.tensor(test_x.to_numpy(), dtype=torch.float32), torch.tensor(test_y.to_numpy(), dtype=torch.float32)
-    #yp_test = torch.tensor(yp.to_numpy()) #, dtype=torch.float32)
+    if m == "res2":
+        yp_test = torch.tensor(yp.to_numpy(), dtype=torch.float32)
     xt_test = torch.tensor(xt_test.to_numpy(), dtype=torch.float32)
 
     preds_test = np.zeros((test_x.shape[0], 4))
@@ -51,7 +52,7 @@ def predict(test_x, test_y, m, data_use, yp, xt_test, current_dir=''):
             model = models.EMB(test_x.shape[1], 1, [[32], [32]], 12, 1)
 
         if m =='mlpDA':
-            model.load_state_dict(torch.load(''.join((data_dir, f"{m}_pretrained_{data_use}_1_trained_model{i}.pth"))))
+            model.load_state_dict(torch.load(''.join((data_dir, f"{m}_pretrained_{data_use}_{N}_{da}_trained_model{i}.pth"))))
         else:
             model.load_state_dict(torch.load(''.join((data_dir, f"{m}_{data_use}_model{i}.pth"))))
 
@@ -78,7 +79,7 @@ def via(data_use, model, prediction_scenario, current_dir =''):
     else:
         make_sparse = False
 
-    x, y, xt, mn, std = utils.loaddata('validation', 1, dir='../../data'), raw=True, sparse=make_sparse, via=True)
+    x, y, xt, mn, std = utils.loaddata('validation', 1, dir='../../data/', raw=True, sparse=make_sparse, via=True)
     yp = pd.read_csv(f"../../data/hyytialaF_{data_use}.csv")
     yp.index = pd.DatetimeIndex(yp['date'])
 
@@ -92,7 +93,7 @@ def via(data_use, model, prediction_scenario, current_dir =''):
                   'SWp': [yp['SWp'].min(), yp['SWp'].max()]
                   }
 
-    gridsize = 200
+    gridsize = 2
     
     if model == 'res2':
         yp = yp.drop(yp.columns.difference(['GPPp']), axis=1)
@@ -104,10 +105,10 @@ def via(data_use, model, prediction_scenario, current_dir =''):
         ypte = yp.drop(yp.columns.difference(['GPPp', 'ETp', 'SWp']), axis=1)
         y = yp.drop(yp.columns.difference(['GPP']), axis=1)
         n = [1,1]
-        x_tr, n = utils.add_history(yptr, n, 1)
-        x_te, n = utils.add_history(ypte, n, 1)
-        x_tr, mn, std = utils.standardize(x_tr, get_p=True)
-        x_te = utils.standardize(x_te, [mn, std])
+        #x_tr, n = utils.add_history(yptr, n, 1)
+        #x_te, n = utils.add_history(ypte, n, 1)
+        x_tr, mn, std = utils.standardize(yptr, get_p=True)
+        x_te = utils.standardize(ypte, [mn, std])
         test_x = x_te[x_te.index.year == 2008]
         test_y = y[y.index.year == 2008][1:]
         variables = ['GPPp', 'ETp', 'SWp']
@@ -125,10 +126,10 @@ def via(data_use, model, prediction_scenario, current_dir =''):
 
     for v in variables:
         # Create variable values over complete ranges and standardize by full dataset
-        if model == 'res':
-            var_range = (np.linspace(thresholds[v][0], thresholds[v][1], gridsize)-mn[''.join((v, '_x'))])/std[''.join((v, '_x'))]
-        else:
-            var_range = (np.linspace(thresholds[v][0], thresholds[v][1], gridsize)-mn[v])/std[v]
+        #if model == 'res':
+        #    var_range = (np.linspace(thresholds[v][0], thresholds[v][1], gridsize)-mn[''.join((v, '_x'))])/std[''.join((v, '_x'))]
+        #else:
+        var_range = (np.linspace(thresholds[v][0], thresholds[v][1], gridsize)-mn[v])/std[v]
 
         dat = test_x.copy()
         # Compute effect of variable at mean of 14 days around record dates for seasonal changes
@@ -142,33 +143,28 @@ def via(data_use, model, prediction_scenario, current_dir =''):
 
         days = via_subset(dat)
         days_yp = via_subset(yp)
-        days_xt = via_subset(test_xt)
-
+        if model == "emb":
+            days_xt = via_subset(test_xt)
+        else:
+            days_xt = via_subset(test_x)
+            
         for mon, df in days.items():
             out_i = []
             for i in var_range:
-                df.loc[:,''.join((v, '_x'))] = i
-                df.loc[:,''.join((v, '_y'))] = i
-
+                if model != 'res':
+                    df.loc[:,''.join((v, '_x'))] = i
+                    df.loc[:,''.join((v, '_y'))] = i
+                else:
+                    df.loc[:,v] = i
+                    df.loc[:,v] = i
                 ps = predict(df, test_y, model, data_use, days_yp[mon], days_xt[mon], current_dir=current_dir)
 
                 out_i.append(ps)
 
-            pd.DataFrame(out_i).to_csv(f'results/{model}_{data_use}_{v}_via_cond_{mon}.csv'))
+            pd.DataFrame(out_i).to_csv(f'./results/{model}_{data_use}_{v}_via_cond_{mon}.csv')
 
 
 if __name__ == '__main__':
 
-    via('sparse', 'emb', prediction_scenario = 'temporal')
-    via('full', 'emb', prediction_scenario = 'temporal')
-    #via('sparse', 'embtest', prediction_scenario = 'temporal')
-    #via('sparse', 'mlp', prediction_scenario = 'temporal')
-    #via('full', 'mlp', prediction_scenario = 'temporal')
-    #via('sparse', 'res2', prediction_scenario = 'temporal')
-    #via('full', 'res2', prediction_scenario='temporal')
-    #via('sparse', 'res', prediction_scenario='temporal')
-    #via('full', 'res', prediction_scenario='temporal')
-    #via('sparse', 'reg', prediction_scenario='temporal')
-    #via('full', 'reg', prediction_scenario='temporal')
-    #via('sparse', 'mlpDA', prediction_scenario='temporal')
-    #via('full', 'mlpDA', prediction_scenario='temporal')
+    via(args.d, args.m, prediction_scenario = 'temporal')
+
