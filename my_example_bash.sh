ae577e0bb50a7fb87c6621d4d18c084acac78170
MWesselkamp <marieke.wesselkamp@posteo.de>
2024-10-08 13:39:26 +0000

    example bash

--- my_example_bash.sh
+++ my_example_bash.sh
@@ -21,10 +21,7 @@ WORKDIR=/home/${USER}/PycharmProjects/PiNNs
 cd $WORKDIR
 module purge
 module load conda
-conda activate /perm/pamw/conda/envs/pinns
+conda activate /your/conda/envs/pinns
 
-srun python3 temporal/nas/ENmlp.py -d full
+srun python3 temporal/nas/ENres.py -d full
 
-#srun python3 src/forecast/run_forecast_global.py mlp_europe_config.yaml lstm_config_sc.yaml xgb_europe_config.yaml # lstm_config.yaml xgb_europe_config.yaml
-#srun python3 src/forecast/run_forecast_global.py --config_file_mlp mlp_europe_config.yaml --config_file_lstm lstm_europe_config.yaml --config_file_xgb xgb_europe_config.yaml # mlp_global_config.yaml lstm_config_sc.yaml xgb_global_config.yaml 
-#srun python3 src/forecast/run_forecast.py --config_file_mlp None --config_file_lstm lstm_global_config.yaml --config_file_xgb None #xgb_global_config.yaml
